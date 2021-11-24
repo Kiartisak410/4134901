fe6e90a319c67c6b1f785c2727289c8d11fceffd
kiartisak410 <om410@hotmail.com>
2021-11-24 11:38:42 +0700

    react

---
+++ LAB3/hello-react/src/components/name.js
@@ -0,0 +1,10 @@
+function Myname() {
+    return (
+        <div>
+            <div>Kiartisak Klomprakhon</div>
+            <div>SID : 610112418003</div>
+        </div>
+    )
+}
+
+export default Myname

81c8a02c82403f6d597aea5cfc85b6c21046347f
kiartisak410 <om410@hotmail.com>
2021-12-15 11:16:35 +0700

    lab6

--- LAB3/hello-react/src/index.js
+++ LAB3/hello-react/src/index.js
@@ -6,6 +6,7 @@ import Car from "./components/Car";
 import List from "./components/List";
 import Brand from "./components/Ul";
 import DataTable from "./components/Table";
+import Hook from "./components/Hook"
 
 ReactDOM.render(
   <>
@@ -13,6 +14,7 @@ ReactDOM.render(
     <Car age="21" eye="black" />
     <List />
     <Brand />
+    <Hook />
   </>,
   document.getElementById("root")
 );
